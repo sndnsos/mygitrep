6dfffc1feb790dc42f2ca4b56f2f08050d207149
wkc <sndnsos@hotmail.com>
2018-07-29 14:02:02 +0800

    add Pure_Head.cpp

---
+++ Pure_Head.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+#include<string>
+#include<ctype.h>
+#include<math.h>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<bitset>
+#include<algorithm>
+#include<time.h>
+using namespace std;
+void fre() { freopen("c://test//input.in", "r", stdin); freopen("c://test//output.out", "w", stdout); }
+#define MS(x, y) memset(x, y, sizeof(x))
+#define ls o<<1
+#define rs o<<1|1
+typedef long long LL;
+typedef unsigned long long UL;
+typedef unsigned int UI;
+template <class T1, class T2>inline void gmax(T1 &a, T2 b) { if (b > a)a = b; }
+template <class T1, class T2>inline void gmin(T1 &a, T2 b) { if (b < a)a = b; }
+const int N = 0, M = 0, Z = 1e9 + 7, inf = 0x3f3f3f3f;
+template <class T1, class T2>inline void gadd(T1 &a, T2 b) { a = (a + b) % Z; }
+int casenum, casei;
+int main()
+{
+	scanf("%d", &casenum);
+	for (casei = 1; casei <= casenum; ++casei)
+	{
+		
+	}
+	return 0;
+}
+/*
+【trick&&吐槽】
+
+
+【题意】
+
+
+【分析】
+
+
+【时间复杂度&&优化】
+
+
+【数据】
+
+
+*/
